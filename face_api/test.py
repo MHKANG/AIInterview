3df0d8fd47b034fa860eeabb7c3a0dda4abec17a
ChunHwa Park <winters0727@gmail.com>
2020-09-29 15:16:34 +0900

    temp commit

--- face_api/test.py
+++ face_api/test.py
@@ -9,13 +9,128 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import os
+from collections import deque, defaultdict
 from torch.autograd import Variable
 import seaborn as sns
+import requests
+import json
 
 import transforms as transforms
 from skimage import io
 from skimage.transform import resize
 from models import *
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+def video_analysis(username, video_type):
+
+    def video_to_image(name):
+        video_folders = os.listdir('./videos')
+        if not name in video_folders:
+            print('There is no video!')
+            return 0
+        
+        image_folders = os.listdir('./images')
+        if not name in image_folders:
+            os.mkdir('./images/{0}'.format(name))
+        name_image_folders = os.listdir('./images/{0}'.format(name))
+        folder_index = len(name_image_folders)+1
+        os.mkdir('./images/{0}/{1}'.format(name, folder_index))
+
+        name_video_folders = os.listdir('./images/{0}'.format(name))
+        video_index = len(name_video_folders)
+        vidcap = cv2.VideoCapture('./videos/{0}/{1}{2}.{3}'.format(name, name, video_index, video_type))
+        count = 0
+        while vidcap.isOpened():
+            ret, image = vidcap.read()
+
+            if ret:
+                if(int(vidcap.get(1)) % 20 == 0):
+                    cv2.imwrite("./images/{0}/{1}/frame{2}.jpg".format(name, folder_index, count), image)
+                    count += 1
+            else:
+                break
+        vidcap.release()
+
+        return folder_index
+
+    def capture_emotion(folder_index, name):
+        images = os.listdir('./images/{0}/{1}'.format(name, folder_index))
+
+        result_list = deque()
+
+        for index in range(len(images)):
+            raw_img = io.imread('images/{0}/{1}/frame{2}.jpg'.format(name, folder_index, index))
+            gray = rgb2gray(raw_img)
+            gray = resize(gray, (48,48), mode='symmetric').astype(np.uint8)
+
+            img = gray[:, :, np.newaxis]
+
+            img = np.concatenate((img, img, img), axis=2)
+            img = Image.fromarray(img)
+            inputs = transform_test(img)
+
+            class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+            ncrops, c, h, w = np.shape(inputs)
+
+            inputs = inputs.view(-1, c, h, w)
+            # inputs = inputs.cuda()
+            inputs = Variable(inputs, volatile=True)
+            outputs = net(inputs)
+
+            outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
+
+            score = F.softmax(outputs_avg)
+            _, predicted = torch.max(outputs_avg.data, 0)
+
+            emotion_list = list()
+            
+            for i in range(len(class_names)):
+                emotion_list.append((class_names[i], score.data.cpu().numpy()[i]))
+            emotion_list.sort(key=lambda x:x[1], reverse=True)
+            emotion_list = deque(emotion_list)
+            
+            result_list.append(emotion_list)
+        
+        return result_list
+
+    def cal_point(result_list):
+        point = 50
+        point_list = list()
+        emo_list = list()
+        while result_list:
+            result = result_list.popleft()
+            most_emotion = result.popleft()
+            emotion, percentage = most_emotion
+            emo_list.append(emotion)
+            if emotion == 'Happy':
+                point += 1.5
+            elif emotion == 'Surpirse':
+                point += 0.5
+            elif emotion == 'Fear':
+                point -= 0.5
+            elif emotion == 'Sad':
+                point -= 1.5
+            elif emotion == 'Angry':
+                point -= 2.5
+            else:
+                pass
+            point_list.append(point)
+        return point, point_list, emo_list
+
+    folder_index = video_to_image(username)
+
+    if folder_index:
+        result = capture_emotion(folder_index, username)
+        
+        point, point_list, emo_list = cal_point(result)
+        result_data = {}
+        return requests.post('http://localhost:8080/api/interviewresult', data = json.dumps(result_data))
+        # print(point)
+        # print(point_list)
+        # print(emo_list)
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -69,6 +184,7 @@ def cvdata(data):
     emotion_list.sort(key=lambda x:x[1], reverse=True)
     print(emotion_list)
     print("cv")
+    return requests.post()
 
 @socketio.on('my event')
 def handle_my_custum_event(json, methods=['GET', 'POST']):
@@ -76,14 +192,15 @@ def handle_my_custum_event(json, methods=['GET', 'POST']):
 
 @socketio.on('uploadFile')
 def uploadFile(data):
-    username, video_file = data['data']['uid'], data['data']['file']
-    video_count = len(os.listdir('./videos/{0}'.format(username)))
-    with open('./videos/{0}/{1}{2}.avi'.format(username, username, video_count), 'wb') as f:
+    username, video_file, video_type = data['data']['uid'], data['data']['file'], data['data']['type'].split('/')[1]
+    user_list = os.listdir('./videos')
+    if username not in user_list:
+        os.mkdir('./videos/{0}'.format(username))
+    video_count = len(os.listdir('./videos/{0}'.format(username)))+1
+    with open('./videos/{0}/{1}{2}.{3}'.format(username, username, video_count, video_type), 'wb') as f:
         f.write(video_file);
 
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
-
+    video_analysis(username, video_type)
 
 cut_size = 44
 
