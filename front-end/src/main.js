59358ffbbc61c098be6664577fbf40bd2dd2be8a
Kang Myun Hun <lizingmaster@gmail.com>
2020-10-05 01:24:29 +0900

    add jwt-auth-token in login

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import store from './store'
+import axios from './axios'
+
 import './plugins/base'
 import { router } from './router'
 import vuetify from './plugins/vuetify';
 import VueSession from 'vue-session'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+
 Vue.use(VueSession)
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
